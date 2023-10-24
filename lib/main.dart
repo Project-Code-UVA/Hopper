0c11dc032a32af5ce9a9848f19bfa8b3c7d74bdf
cassiebux22 <rce6ab@virginia.edu>
2023-10-23 21:00:19 -0400

    Update main.dart

---
+++ lib/main.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'firebase_options.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+//note about importing 'firebase_options.dart' and using it within the main method
+//had to first run npm install -g firebase-tools
+//then ran firebase login to login to the firebase
+//after i ran flutterfire configure and chose "Hopper" and then "android"
+//good resource: https://stackoverflow.com/questions/72895721/firebasecloudmessaging-platformexception-platformexceptionnull-error-host-p 
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  //reference to cloud firebase
+  CollectionReference refBarList = FirebaseFirestore.instance.collection('Bars');
+  //Stream to hold list of items; "late" because we will initialize it later
+  late Stream<QuerySnapshot> streamBarInfo;
+
+  //we don't want to call snapshot() everytime build runs so we just run it in the initial state
+  @override
+  initState(){
+    super.initState();
+
+    streamBarInfo = refBarList.snapshots();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    //two ways to get fruit list
+    // refFruitList.get(); //one time read 
+    // refFruitList.snapshots(); //returns stream, which gives us list of documents continuously
+    //difference between the two is that we can listen to the stream but not the future (get()), thus we can listen to realtime updates with snapshots().
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: streamBarInfo,
+        builder: (BuildContext context, AsyncSnapshot snapshot){
+          //if error, return error
+          if(snapshot.hasError){
+            return Center(child: Text(snapshot.error.toString()));
+          }
+          //if connected, fetch data
+          if(snapshot.connectionState==ConnectionState.active){
+            QuerySnapshot querySnapshot = snapshot.data;
+            List<QueryDocumentSnapshot> listQueryDocumentSnapshot = querySnapshot.docs;
+
+            return ListView.builder(
+              itemCount: listQueryDocumentSnapshot.length,
+              itemBuilder: (content, index){
+                QueryDocumentSnapshot document = listQueryDocumentSnapshot[index];
+                return Text(document['name']);
+              }
+            );
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      )
+    );
+  }
+  // @override
+  // Widget build(BuildContext context) {
+  //   // This method is rerun every time setState is called, for instance as done
+  //   // by the _incrementCounter method above.
+  //   //
+  //   // The Flutter framework has been optimized to make rerunning build methods
+  //   // fast, so that you can just rebuild anything that needs updating rather
+  //   // than having to individually change instances of widgets.
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       // TRY THIS: Try changing the color here to a specific color (to
+  //       // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+  //       // change color while the other colors stay the same.
+  //       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+  //       // Here we take the value from the MyHomePage object that was created by
+  //       // the App.build method, and use it to set our appbar title.
+  //       title: Text(widget.title),
+  //     ),
+  //     body: Center(
+  //       // Center is a layout widget. It takes a single child and positions it
+  //       // in the middle of the parent.
+  //       child: Column(
+  //         // Column is also a layout widget. It takes a list of children and
+  //         // arranges them vertically. By default, it sizes itself to fit its
+  //         // children horizontally, and tries to be as tall as its parent.
+  //         //
+  //         // Column has various properties to control how it sizes itself and
+  //         // how it positions its children. Here we use mainAxisAlignment to
+  //         // center the children vertically; the main axis here is the vertical
+  //         // axis because Columns are vertical (the cross axis would be
+  //         // horizontal).
+  //         //
+  //         // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+  //         // action in the IDE, or press "p" in the console), to see the
+  //         // wireframe for each widget.
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: <Widget>[
+  //           const Text(
+  //             'You have pushed the button this many times:',
+  //           ),
+  //           Text(
+  //             '$_counter',
+  //             style: Theme.of(context).textTheme.headlineMedium,
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //     floatingActionButton: FloatingActionButton(
+  //       onPressed: _incrementCounter,
+  //       tooltip: 'Increment',
+  //       child: const Icon(Icons.add),
+  //     ), // This trailing comma makes auto-formatting nicer for build methods.
+  //   );
+  // }
+}
