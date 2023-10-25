2f91825e2405a06ff43eb4cbefa294c9a4eea533
srgeddes <rileygeddes22@gmail.com>
2023-10-25 00:50:35 -0400

    nothing

--- lib/Models/listpage.dart
+++ lib/Models/listpage.dart
@@ -19,43 +19,6 @@ class ListPage extends StatelessWidget {
   }
 }
 
-class TransparentRoute extends PageRouteBuilder {
-  final Widget page;
-
-  TransparentRoute({required this.page})
-      : super(
-          pageBuilder: (
-            context,
-            animation,
-            secondaryAnimation,
-          ) =>
-              page,
-          transitionsBuilder: (
-            context,
-            animation,
-            secondaryAnimation,
-            child,
-          ) {
-            const begin = Offset(0, 1);
-            const end = Offset.zero;
-            const curve = Curves.easeInOut;
-
-            var tween =
-                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-            var offsetAnimation = animation.drive(tween);
-
-            return SlideTransition(
-              position: offsetAnimation,
-              child: Opacity(
-                opacity: animation.value,
-                child: child,
-              ),
-            );
-          },
-        );
-}
-
 class MyBottomAppBar extends StatelessWidget {
   const MyBottomAppBar({Key? key});
 
