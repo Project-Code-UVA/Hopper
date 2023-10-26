90c2c3b0941046447daeca2d2e33f88cb9b8688f
srgeddes <rileygeddes22@gmail.com>
2023-10-25 20:55:14 -0400

    New Listpage

--- lib/Models/listpage.dart
+++ lib/Models/listpage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hopper/Views/main.dart';
+import 'package:hopper/Models/mapscreen.dart';
 
 class ListPage extends StatelessWidget {
   const ListPage({Key? key});
@@ -7,25 +7,84 @@ class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('List Page'),
-        backgroundColor: const Color.fromRGBO(35, 45, 75, 1),
-      ),
       body: Stack(
         children: [
-          MapScreen(), // Keep the MapScreen here
-          Opacity(
-            opacity: 0.7, // Adjust this value for the desired transparency
-            child: Container(
-              color: Colors.black, // Set the background color here
+          Positioned(
+            child: Center(
+              child: Transform.scale(
+                scale: 1.1, // Adjust the scale factor as needed
+                child: Image.asset('assets/Default_Map.jpeg'),
+              ),
             ),
           ),
-          Center(
-            child: Text('This is the List Page content'),
+          Opacity(
+            opacity: 0.7,
+            child: Container(
+              color: Colors.black,
+            ),
           ),
+          _buildRoundedRectangles(),
         ],
       ),
       bottomNavigationBar: const MyBottomAppBar(),
+    );
+  }
+
+  Widget _buildRoundedRectangles() {
+    return Container(
+      margin: EdgeInsets.only(top: 50.0),
+      child: ListView(
+        // Wrap the list with a ListView for scrolling
+        children: <Widget>[
+          CustomRoundedRectangle(
+            title: 'Trin',
+          ),
+          CustomRoundedRectangle(
+            title: 'Coupes',
+          ),
+          // Add more instances of CustomRoundedRectangle with different content
+        ],
+      ),
+    );
+  }
+}
+
+class CustomRoundedRectangle extends StatelessWidget {
+  final String title;
+
+  CustomRoundedRectangle({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: 0.6,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(12.0),
+          child: Container(
+            width: 400.0,
+            height: 150.0,
+            color: Colors.black,
+            child: Center(
+              child: ListTile(
+                title: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -43,7 +102,6 @@ class MyBottomAppBar extends StatelessWidget {
           IconButton(
             iconData: Icons.home,
             onTap: () {
-              // Use Navigator to pop the current screen and go back to the previous one
               Navigator.of(context).pop();
             },
           ),
--- lib/Models/mapscreen.dart
+++ lib/Models/mapscreen.dart
@@ -1,38 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapScreen extends StatefulWidget {
-  final double initialLatitude;
-  final double initialLongitude;
-
-  MapScreen({
-    required this.initialLatitude,
-    required this.initialLongitude,
-  });
-
-  @override
-  _MapScreenState createState() => _MapScreenState();
-}
-
-class _MapScreenState extends State<MapScreen> {
+class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Google Maps'),
-      ),
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(widget.initialLatitude, widget.initialLongitude),
-          zoom: 15.0,
+    const initialLat = 38.0345;
+    const intialLong = -78.4990;
+    return Center(
+      child: RepaintBoundary(
+        child: FutureBuilder(
+          future: rootBundle.loadString('assets/dark_map_style.json'),
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.hasData) {
+              return GoogleMap(
+                initialCameraPosition: const CameraPosition(
+                  target: LatLng(initialLat, intialLong),
+                  zoom: 16.2,
+                ),
+                onMapCreated: (GoogleMapController controller) {
+                  controller.setMapStyle(snapshot.data!);
+                },
+              );
+            } else {
+              return const CircularProgressIndicator();
+            }
+          },
         ),
-        markers: {
-          Marker(
-            markerId: MarkerId('marker_id'),
-            position: LatLng(widget.initialLatitude, widget.initialLongitude),
-            infoWindow: InfoWindow(title: 'Marker Title'),
-          ),
-        },
       ),
     );
   }
