e9d55e1d9273f4a4740cd22b771e30604e13be72
srgeddes <rileygeddes22@gmail.com>
2023-10-27 16:18:38 -0400

    no more debug banner

--- lib/Views/main.dart
+++ lib/Views/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // Get rid of debug banner
       home: Scaffold(
         backgroundColor: Colors.transparent,
         body: MapScreen(),
