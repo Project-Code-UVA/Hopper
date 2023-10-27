7e5a8e94cf65adb370e11c53cd782e8f403ce515
srgeddes <rileygeddes22@gmail.com>
2023-10-27 16:15:20 -0400

    new button map screen

--- lib/Models/mapscreen.dart
+++ lib/Models/mapscreen.dart
@@ -2,31 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
+  @override
+  _MapScreenState createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  final initialLat = 38.0345;
+  final initialLong = -78.4990;
+
+  GoogleMapController? _controller;
+
   @override
   Widget build(BuildContext context) {
-    const initialLat = 38.0345;
-    const intialLong = -78.4990;
     return Center(
-      child: RepaintBoundary(
-        child: FutureBuilder(
-          future: rootBundle.loadString('assets/dark_map_style.json'),
-          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-            if (snapshot.hasData) {
-              return GoogleMap(
-                initialCameraPosition: const CameraPosition(
-                  target: LatLng(initialLat, intialLong),
-                  zoom: 16.2,
-                ),
-                onMapCreated: (GoogleMapController controller) {
-                  controller.setMapStyle(snapshot.data!);
+      child: Stack(
+        children: [
+          FutureBuilder(
+            future: rootBundle.loadString('assets/dark_map_style.json'),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.hasData) {
+                return GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(initialLat, initialLong),
+                    zoom: 16.2,
+                  ),
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller = controller;
+                    controller.setMapStyle(snapshot.data!);
+                  },
+                );
+              } else {
+                return CircularProgressIndicator();
+              }
+            },
+          ),
+          Align(
+            alignment:
+                Alignment(1.035, 1.0154), // Adjust these values for positioning
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: FloatingActionButton(
+                onPressed: () {
+                  if (_controller != null) {
+                    CameraPosition newPosition = CameraPosition(
+                      target: LatLng(initialLat, initialLong),
+                      zoom: 16.2,
+                    );
+                    _controller!.animateCamera(
+                        CameraUpdate.newCameraPosition(newPosition));
+                  }
                 },
-              );
-            } else {
-              return const CircularProgressIndicator();
-            }
-          },
-        ),
+                backgroundColor: const Color.fromRGBO(
+                    0, 0, 0, 1), // Change the background color here
+                foregroundColor: const Color.fromRGBO(
+                    229, 114, 0, 1), // Change the icon color here
+                elevation:
+                    4.0, // Adjust the elevation for a slightly bigger button
+                child: Icon(Icons.location_searching),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
